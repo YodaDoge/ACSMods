4a8b3499175f9790b70a063d41cc96a8f932a561
YodaDoge <51869140+YodaDoge@users.noreply.github.com>
2026-04-30 07:54:20 +0200

    Multi Char Selection for eatiItem

--- Source/UI/CharacterSelection.cs
+++ Source/UI/CharacterSelection.cs
@@ -13,20 +13,53 @@ using XiaWorld.UI.InGame;
 
 namespace ACS_Yoda_Tweaks
 {
-	public class SkipCharacterPrompt : Mod
+	public class CharacterSelection : Mod
 	{
 		public override Meta Info => _info;
 		private static Meta _info = new Meta("BetterCharSelect", "Always use solo disciple", true);
 
 		private static bool _pauseAfterLoad = false;
 
-		public SkipCharacterPrompt(bool defaultEnabled) : base(defaultEnabled)
+		public CharacterSelection(bool defaultEnabled) : base(defaultEnabled)
 		{
 		}
 
 		[HarmonyPatch]
 		public static class Patch
 		{
+			[HarmonyPostfix]
+			[HarmonyPatch(typeof(Wnd_GameMain), "OnInit")]
+			public static void OnInit_Postfix(Wnd_GameMain __instance)
+			{
+				try
+				{
+					var itemCmds = ThingUICommandDefine.sThingUICommands.GetValueSafe(g_emSelectThingSort.Item);
+					var eatCmd = itemCmds.FirstOrDefault(x => x.Name == TFMgr.Get("食用给与")); //eat
+					eatCmd.Act = delegate (Thing t, AreaBase a)
+					{
+						ItemThing item = t as ItemThing;
+						Wnd_SelectNpc.Instance.Select(delegate (List<int> npcids)
+						{
+							if (npcids != null && npcids.Count > 0)
+							{
+								foreach (int npcid in npcids)
+								{
+									item.DoDropFromBag();
+									TongLingMgr.Instance.TriggerJingGuaiStory("JGRuin", t, runaway: true);
+									Npc npc = ThingMgr.Instance.FindThingByID(npcid) as Npc;
+									npc.AddCommand("EatItem", item);
+								}
+							}
+						}, maxcount: item.FreeCount);
+					};
+				}
+				catch (Exception ex)
+				{
+					ShowMessage(ex);
+				}
+			}
+
+
 			[HarmonyPostfix]
 			[HarmonyPatch(typeof(Wnd_SelectNpc), "OnShowUpdate")]
 			[HarmonyPriority(1000)]
