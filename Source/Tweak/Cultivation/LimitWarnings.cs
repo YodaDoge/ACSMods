1d8f5a39a417fcc7b17a9e08202bf013b939f0c4
YodaDoge <51869140+YodaDoge@users.noreply.github.com>
2026-05-03 15:39:26 +0200

    Warning Symbol stay on character bar

--- Source/Tweak/Cultivation/LimitWarnings.cs
+++ Source/Tweak/Cultivation/LimitWarnings.cs
@@ -3,6 +3,7 @@ using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
+using UnityEngine;
 using XiaWorld;
 using static XiaWorld.AuctionData;
 using static XiaWorld.OutspreadMgr;
@@ -15,29 +16,63 @@ namespace ACS_Yoda_Tweaks
 		public static class PatchLimits
 		{
 			[HarmonyPostfix]
-			[HarmonyPatch(typeof(CommandGoMapExplore), "CouldBeFind")]
-			public static void StartAdventure(CommandGoMapExplore __instance, Npc npc)
+			[HarmonyPatch(typeof(Thing), "AddCommand")]
+			public static void ExploreCommandRemoveExplorationNeckwarning(Thing __instance, string type, params object[] param)
 			{
-				if (HasAdventureNeck(npc))
+				if (!(__instance is Npc npc) || !npc.IsPlayerThing || !npc.IsSmartRace)
+					return;
+
+				if (HasAdventureNeck(npc) && type == "GoMapExplore")
 				{
-					var msg = MessageMgr.Instance.m_mapLevelMsg.FirstOrDefault(x => x._AttributID == 35 && x._ThingID?.Contains(npc.ID) == true);
-					if (msg != null)
-					{
-						MessageMgr.Instance.RemoveMessage(35, new List<Thing> { npc });
-						Wnd_MessageBox.Instance.UpdateMessage();
-					}
+					RemoveLimitWarning(npc);
 				}
 			}
+
+			private static void RestoreLimitWarning(Npc npc)
+			{
+				MessageMgr.Instance.AddMessage(35, new List<Thing> { npc });
+				Wnd_MessageBox.Instance.UpdateMessage();
+			}
+
+			private static void RemoveLimitWarning(Npc npc)
+			{
+				var msg = MessageMgr.Instance.m_mapLevelMsg.FirstOrDefault(x => x._AttributID == 35 && x._ThingID?.Contains(npc.ID) == true);
+				if (msg != null)
+				{
+					msg._OnlyBox = true;
+					//MessageMgr.Instance.RemoveMessage(35, new List<Thing> { npc });
+					Wnd_MessageBox.Instance.UpdateMessage();
+				}
+			}
+
+			[HarmonyPostfix]
+			[HarmonyPatch(typeof(Command), "FinishCommand")]
+			public static void FinishCommand(Command __instance, bool del = false, bool debug = false, bool mustRemove = false)
+			{
+				if (!(__instance.OwnerThing is Npc npc) || !(__instance is CommandGoMapExplore cmd))
+					return;
+
+				if (HasAdventureNeck(npc))
+				{
+					RestoreLimitWarning(npc);
+				}
+			}
+
+				//[HarmonyPostfix]
+				//[HarmonyPatch(typeof(CommandGoMapExplore), "CouldBeFind")]
+				//public static void StartAdventure(CommandGoMapExplore __instance, Npc npc)
+				//{
+
+				//}
 
 			[HarmonyPostfix]
 			[HarmonyPatch(typeof(JobLeave2Explore), "OnLeaveJob")]
 			public static void RestoreLimitMessageOnFail(JobLeave2Explore __instance, KStateQUnit unit)
 			{
 				var npc = __instance.Worker;
-				if (HasAdventureNeck(npc) && npc.CheckCommand("CommandGoMapExplore", checkcount:true)?.Any(x => x != null) != true)
+				if (HasAdventureNeck(npc) && npc.CheckCommand("GoMapExplore", checkcount: true)?.Any(x => x != null) != true)
 				{
-					MessageMgr.Instance.AddMessage(35, new List<Thing> { npc });
-					Wnd_MessageBox.Instance.UpdateMessage();
+					RestoreLimitWarning(npc);
 				}
 			}
 
@@ -67,15 +102,7 @@ namespace ACS_Yoda_Tweaks
 			[HarmonyPatch(typeof(JobBrokenNeck), "OnEnterJob")]
 			public static void OnEnterBCToil(JobBrokenNeck __instance, KStateQUnit unit)
 			{
-				if (MessageMgr.UseOldMessage)
-				{
-					MsgMgr.Instance.RemoveMsg(26, __instance.Worker);
-				}
-				else
-				{
-					MessageMgr.Instance.RemoveMessage(35, new List<Thing> { __instance.Worker });
-				}
-
+				RemoveLimitWarning(__instance.Worker);
 			}
 
 			[HarmonyPostfix]
@@ -86,14 +113,9 @@ namespace ACS_Yoda_Tweaks
 
 				if (fail)
 				{
-					if (MessageMgr.UseOldMessage)
-					{
-						MsgMgr.Instance.AddMsg(26, __instance.Worker);
-					}
-					else
-					{
-						MessageMgr.Instance.AddMessage(35, new List<Thing> { __instance.Worker });
-					}
+				//handled by message "failed breakthrough";
+					//RestoreLimitWarning(npc);
+					//RemoveLimitWarning(__instance.Worker);
 				}
 			}
 
